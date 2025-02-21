8a904a49cb1d665719b7e2f4bd0d030084b7c15a
dennischou <dses5900@gmail.com>
2025-02-21 15:50:24 +0800

    Add for debug

--- common/scripts/load-k8s-secrets.sh
+++ common/scripts/load-k8s-secrets.sh
@@ -13,4 +13,4 @@ PATTERNPATH=$(dirname "${COMMONPATH}")
 
 PATTERN_NAME=${1:-$(basename "`pwd`")}
 
-ansible-playbook -e pattern_name="${PATTERN_NAME}" -e pattern_dir="${PATTERNPATH}" "rhvp.cluster_utils.k8s_secrets"
+ansible-playbook -e pattern_name="${PATTERN_NAME}" -e pattern_dir="${PATTERNPATH}" -e override_no_log=false "rhvp.cluster_utils.k8s_secrets"
